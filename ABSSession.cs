bcf7bc23794cbe6a23567df968abc379d432a618
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-06 15:57:56 +0800

    Add MessageProcess

--- ABSSession.cs
+++ ABSSession.cs
@@ -10,6 +10,7 @@ public interface ISession
 {
     IEnumerable<IEntity> Entities { get; }
     IEnumerable<IEvent> OnNextTurn();
+    void OnMessage(IMessage message);
 
     IModder Modder { get; set; }
 }
@@ -27,18 +28,53 @@ public abstract class Entity : IEntity
 
 public interface IEvent
 {
-
+    IEntity From { get; }
+    IEntity To { get; }
+    IEventDef Def { get; }
 }
 
 public interface IEventDef
 {
     public ICondition Condition { get; }
     public ITargetFinder TargetFinder { get; }
+
+    public IOption Option { get; }
+}
+
+public interface IOption
+{
+    public IEnumerable<IMessageBind> MessageBinds { get; }
+}
+
+public class Option : IOption
+{
+    public IEnumerable<IMessageBind> MessageBinds { get; set; }
+}
+
+public interface IMessageBind
+{
+    public Type MessageType { get; }
+
+    public DataVisitor TargetVisitor { get; }
+    public DataVisitor ValueVisitor { get; }
+}
+
+public class MessageBind : IMessageBind
+{
+    public Type MessageType { get; set; }
+
+    public DataVisitor TargetVisitor { get; set; }
+    public DataVisitor ValueVisitor { get; set; }
+}
+
+public interface DataVisitor
+{
+    object Get(IEvent @event);
 }
 
 public interface ITargetFinder
 {
-    IEnumerable<ICondtionFactor> ConditionFactors {  get; }
+    IEnumerable<ICondtionFactor> ConditionFactors { get; }
 
     IEventTarget Targets { get; }
     IEntity Find(IEntity entity, ABSSession session);
@@ -51,9 +87,9 @@ public interface IEventTarget
 
 public interface ICondtionFactor
 {
-    public ICondition Condition { get;  }
+    public ICondition Condition { get; }
 
-    public double Factor { get;  }
+    public double Factor { get; }
 }
 
 public interface ICondition
@@ -97,6 +133,7 @@ public abstract class ABSSession : ISession
     public IModder Modder { get; set; }
 
     private Random random = new Random();
+    public Dictionary<Type, Action<IMessage>> dictMessageProcess = new Dictionary<Type, Action<IMessage>>();
 
     public IEnumerable<IEvent> OnNextTurn()
     {
@@ -112,22 +149,22 @@ public abstract class ABSSession : ISession
                 IEntity target = null;
                 if (eventDef.TargetFinder != null)
                 {
-                    foreach(var item in eventDef.TargetFinder.Targets.Get(entity, this))
+                    foreach (var item in eventDef.TargetFinder.Targets.Get(entity, this))
                     {
                         var factorSum = eventDef.TargetFinder.ConditionFactors.Where(x => x.Condition.IsSatisfied(item, this))
                             .Sum(x => x.Factor);
-                        if(factorSum < 0)
+                        if (factorSum < 0)
                         {
                             continue;
                         }
-                        if(random.NextDouble()  < factorSum)
+                        if (random.NextDouble() < factorSum)
                         {
-                            target = item; 
+                            target = item;
                             break;
                         }
                     }
 
-                    if(target == null)
+                    if (target == null)
                     {
                         continue;
                     }
@@ -145,18 +182,53 @@ public abstract class ABSSession : ISession
             }
         }
     }
+
+    public void OnMessage(IMessage message)
+    {
+        dictMessageProcess[message.GetType()].Invoke(message);
+    }
 }
 
-internal class Event : IEvent
+public class Event : IEvent
 {
+    public static Action<IMessage> ProcessMessage;
+
+    public IEntity From { get; set; }
+
+    public IEntity To { get; set; }
+
+    public IEventDef Def { get; set; }
+
     public Event(IEntity from, IEntity to, IEventDef def)
     {
+        this.From = from;
+        this.To = to;
+        this.Def = def;
+    }
+
+
+    public void DoOption()
+    {
+        var messageBinds = Def.Option.MessageBinds;
+        foreach (var bind in messageBinds)
+        {
+            var message = Activator.CreateInstance(bind.MessageType) as IMessage;
+            message.Target = bind.TargetVisitor.Get(this);
+            message.Value = bind.ValueVisitor.Get(this);
+            ProcessMessage(message);
+        }
     }
 
     internal void AIDo()
     {
-
+        DoOption();
     }
+}
+
+public interface IMessage
+{
+    public object Target { get; set; }
+    public object Value { get; set; }
 }
 
 public class TrueCondtion : ICondition
