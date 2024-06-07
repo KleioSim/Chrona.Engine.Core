b5883b458f7e3f310ccb1e9b067d8e7be1e65110
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-07 16:47:49 +0800

    Add Mod loader

--- Modders/Modder.cs
+++ Modders/Modder.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
 using Chrona.Engine.Core.Events;
 using Chrona.Engine.Core.Interfaces;
 using Chrona.Engine.Core.Sessions;
@@ -7,14 +9,35 @@ namespace Chrona.Engine.Core.Modders;
 
 public class Modder : IModder
 {
-    public Dictionary<Type, IEnumerable<IEventDef>> EventDefs { get; }
+    public Dictionary<Type, IEnumerable<IEventDef>> EventDefs { get; } = new Dictionary<Type, IEnumerable<IEventDef>>();
 
-    public Modder(string modPath)
+    public Modder(string modRootPath)
     {
-        EventDefs = AppDomain.CurrentDomain.GetAssemblies().
-            SelectMany(assm => assm.GetExportedTypes()
-                                  .Where(x => x.IsAssignableTo(typeof(IEventDef)) && x.GetCustomAttribute<DefToAttribute>() != null))
-            .GroupBy(x => x.GetCustomAttribute<DefToAttribute>().toType)
-            .ToDictionary(k => k.Key, v => v.Select(type => Activator.CreateInstance(type) as IEventDef));
+
+        foreach (var modPath in Directory.EnumerateDirectories(modRootPath))
+        {
+            var dllPath = Path.Combine(modPath, "dll", Path.GetFileName(modPath) + ".dll");
+            if (!File.Exists(dllPath))
+            {
+                continue;
+            }
+
+            var alc = AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly());
+            var assembly = alc.LoadFromAssemblyPath(dllPath);
+
+            var types = assembly.ExportedTypes;
+
+            foreach (var eventType in types.Where(x => x.IsAssignableTo(typeof(IEventDef)) && x.GetCustomAttribute<DefToAttribute>() != null))
+            {
+                var toType = eventType.GetCustomAttribute<DefToAttribute>().toType;
+                if (!EventDefs.ContainsKey(toType))
+                {
+                    EventDefs.Add(toType, new List<IEventDef>());
+                }
+
+                var list = EventDefs[toType] as List<IEventDef>;
+                list.Add(Activator.CreateInstance(eventType) as IEventDef);
+            }
+        }
     }
 }
